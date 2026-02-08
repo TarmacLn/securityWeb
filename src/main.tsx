c4228280d7836dbccf259b53648b2c2dff3499d2
OGecgo <oian72116@gmail.com>
2026-02-08 02:34:16 +0200

    end csp

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
 import { createRoot } from 'react-dom/client';
+import createCache from '@emotion/cache';
+import { CacheProvider } from '@emotion/react';
 import './index.css';
 
 import Entry from './index.tsx';
@@ -9,4 +11,17 @@ if (!rootElement) {
     throw new Error('Root element not found');
 }
 
-createRoot(rootElement).render(<Entry />);
+// from mui doc
+const nonce = window.__CSP_NONCE__;
+
+const emotionCache = createCache({
+    key: 'mui',
+    nonce,
+    prepend: true,
+});
+
+createRoot(rootElement).render(
+    <CacheProvider value={emotionCache}>
+        <Entry />
+    </CacheProvider>
+);
